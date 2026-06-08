b3b1847bf9e775d30eb5f0d068f8b406c1926c04
Stephen Cheng <zintaen@gmail.com>
2026-06-08 16:23:43 +0700

    chore: cleanup .cyberos-memory and temporary scripts

--- test-pubchem.js
+++
@@ -1,8 +0,0 @@
-fetch('https://pubchem.ncbi.nlm.nih.gov/rest/pug/periodictable/JSON', {
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36'
-  }
-})
-  .then(res => res.json())
-  .then(data => console.log(data.Table.Columns))
-  .catch(console.error);
